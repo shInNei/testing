2f6025a77679dba15a4bf208f7b1ccea4fe63e7a
Thịnh Nguyễn <dst15092004@gmail.com>
2025-04-24 12:33:24 +0700

    tt3

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: 'postgres.railway.internal',
       port: 5432,
       username: 'postgres',
-      password: 'Thinh@2004',
-      database: 'iqtranhdau',
+      password: 'QRlPGbXOkEXfFJntiAvDNyeIubUXkoGT',
+      database: 'railway',
       entities: [],
       synchronize: true
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
