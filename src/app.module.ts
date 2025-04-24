f3bcd9f2e95488fe04bcf81d36be22192c78cc24
Thịnh Nguyễn <dst15092004@gmail.com>
2025-04-24 13:37:16 +0700

    fixing

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { UserModule } from './user/user.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'postgres.railway.internal',
-      port: 8080,
+      port: 5432,
       username: 'postgres',
       password: 'QRlPGbXOkEXfFJntiAvDNyeIubUXkoGT',
       database: 'railway',
