cf278efe72c4092fb23e2f3b31594911b575f42d
Thịnh Nguyễn <dst15092004@gmail.com>
2025-04-25 22:04:24 +0700

    fixing error

--- prisma/prisma.service.ts
+++ prisma/prisma.service.ts
@@ -2,10 +2,10 @@ import {Injectable} from '@nestjs/common';
 import {PrismaClient} from '@prisma/client';
 
 @Injectable()
-export class PrimService extends PrismaClient {
+export class PrismaService extends PrismaClient {
     constructor() {
         super({
-            datasource: {
+            datasources: {
                 db: {
                     url: process.env.DATABASE_URL,
                 },
